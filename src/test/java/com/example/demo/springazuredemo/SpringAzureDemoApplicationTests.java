88e2a672a0834bdca7b74e68f5b13dfe98ada54b
sujittekale09 <sujittekale09@gmail.com>
2021-11-17 11:18:31 +0530

    first commit

---
+++ src/test/java/com/example/demo/springazuredemo/SpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.springazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
